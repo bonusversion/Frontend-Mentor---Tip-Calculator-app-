b5b47a4a349072ef35f5c288cc10911f6647f852
zkj <438101845@qq.com>
2021-12-01 23:29:44 +0800

    [zkj]feat: check the input is int number

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // Element
 
@@ -7,64 +7,90 @@ const tipValue = document.querySelector(".tip-amount-value");
 const totalValue = document.querySelector(".total-value");
 const peopleNum = document.querySelector(".people-input");
 const customTip = document.querySelector("#custom-tip");
-const resetBt = document.querySelector('.reset');
+const resetBt = document.querySelector(".reset");
 
+const calcTips = function (tipRatio) {
+  let currentTipRatio = parseInt(tipRatio) / 100;
+  if (inputBill.value && peopleNum.value) {
+    const tip =
+      (Number(inputBill.value) * currentTipRatio) / Number(peopleNum.value);
+    const total = tip + inputBill.value / Number(peopleNum.value);
+    tipValue.textContent = tip.toFixed(2);
+    totalValue.textContent = total.toFixed(2);
+  }
+};
 
-const calcTips = function(tipRatio) {
-    let currentTipRatio = parseInt(tipRatio) / 100;
-    if (!inputBill.value !== true && !peopleNum !== true) {
-        const tip = Number(inputBill.value) * currentTipRatio / Number(peopleNum.value)
-        const total = tip + inputBill.value / Number(peopleNum.value);
-        tipValue.textContent = tip.toFixed(2);
-        totalValue.textContent = total.toFixed(2);
-    }
-}
-
-document.querySelectorAll('.tip-box').forEach((tipBox => tipBox.addEventListener('click', function(event) {
-    resetBt.classList.add('reset-active')
-    document.querySelectorAll('.tip-box').forEach(tipBox => tipBox.classList.remove('tip-box-click'));
-    customTip.classList.remove('custom-tip-active');
-    customTip.placeholder = 'Custom';
-    customTip.value = '';
-    event.target.classList.add('tip-box-click');
+document.querySelectorAll(".tip-box").forEach((tipBox) =>
+  tipBox.addEventListener("click", function (event) {
+    resetBt.classList.add("reset-active");
+    document
+      .querySelectorAll(".tip-box")
+      .forEach((tipBox) => tipBox.classList.remove("tip-box-click"));
+    customTip.classList.remove("custom-tip-active");
+    customTip.placeholder = "Custom";
+    customTip.value = "";
+    event.target.classList.add("tip-box-click");
     if (!Number(peopleNum.value)) {
-        document.querySelector('.alert').classList.remove('hidden');
+      document.querySelector(".alert").classList.remove("hidden");
     } else {
-        document.querySelector('.alert').classList.add('hidden');
-        calcTips(event.target.textContent);
+      document.querySelector(".alert").classList.add("hidden");
+      calcTips(event.target.textContent);
     }
-})));
+  })
+);
 
-
-customTip.addEventListener('click', function(event) {
-    resetBt.classList.add('reset-active')
-    document.querySelectorAll('.tip-box').forEach(tipBox => tipBox.classList.remove('tip-box-click'));
-    customTip.classList.add('custom-tip-active');
-    customTip.placeholder = '';
-    if (!Number(peopleNum.value)) {
-        document.querySelector('.alert').classList.remove('hidden');
-    } else {
-        document.querySelector('.alert').classList.add('hidden');
-        calcTips(event.target.value);
-    }
+customTip.addEventListener("click", function (event) {
+  resetBt.classList.add("reset-active");
+  document
+    .querySelectorAll(".tip-box")
+    .forEach((tipBox) => tipBox.classList.remove("tip-box-click"));
+  customTip.classList.add("custom-tip-active");
+  customTip.placeholder = "";
+  if (!Number(peopleNum.value)) {
+    document.querySelector(".alert").classList.remove("hidden");
+  } else {
+    document.querySelector(".alert").classList.add("hidden");
+    calcTips(event.target.value);
+  }
 });
 
+function isInputInt(text) {
+  const number = Number(text);
+  if (number.toString().length !== text.length) {
+    alert("please input int number");
+    return false;
+  }
 
-peopleNum.addEventListener('input', function() {
-    resetBt.classList.add('reset-active');
-})
+  return true;
+}
 
-inputBill.addEventListener('input', function() {
-    resetBt.classList.add('reset-active');
-})
+peopleNum.addEventListener("input", function () {
+  if (!isInputInt(peopleNum.value)) {
+    peopleNum.value = "";
+    return;
+  }
 
-resetBt.addEventListener('click', function() {
-    document.querySelectorAll('.tip-box').forEach(tipBox => tipBox.classList.remove('tip-box-click'));
-    customTip.placeholder = 'Custom';
-    customTip.value = '';
-    inputBill.value = '';
-    peopleNum.value = '';
-    tipValue.textContent = 0;
-    totalValue.textContent = 0;
-    resetBt.classList.remove('reset-active');
-})
+  resetBt.classList.add("reset-active");
+});
+
+inputBill.addEventListener("input", function () {
+  if (!isInputInt(inputBill.value)) {
+    inputBill.value = "";
+    return;
+  }
+
+  resetBt.classList.add("reset-active");
+});
+
+resetBt.addEventListener("click", function () {
+  document
+    .querySelectorAll(".tip-box")
+    .forEach((tipBox) => tipBox.classList.remove("tip-box-click"));
+  customTip.placeholder = "Custom";
+  customTip.value = "";
+  inputBill.value = "";
+  peopleNum.value = "";
+  tipValue.textContent = 0;
+  totalValue.textContent = 0;
+  resetBt.classList.remove("reset-active");
+});
